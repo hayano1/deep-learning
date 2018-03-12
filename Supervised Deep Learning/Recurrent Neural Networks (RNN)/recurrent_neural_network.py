258f1adc4e0ed1ef2e2ce46770d07d0a5bfac2a2
ngilmore <neal.gilmore@gmail.com>
2018-03-12 14:17:53 -0400

    recurrent neural network (RNN)

---
+++ Supervised Deep Learning/Recurrent Neural Networks (RNN)/recurrent_neural_network.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 14:02:38 2018
+
+@author: ngilmore
+"""
+
+# Deep Learning: Stacked LSTM Recurrent Neural Network (RNN) Model in Python
+
+# Predict Google stock price using a Long-Short Term Memory (LSTM) Recurrent Neural Network (RNN) 
+
+# Part 1: Data Preprocessing
+
+# Import required libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values # crease a numpy array of 1 column that we care about - Google Stock Price
+
+
+# Part 2: Build the Recurrent Neural Network (RNN) Model
+
+
+
+# Part 3: Make Prediction and Visualize the Results
+
