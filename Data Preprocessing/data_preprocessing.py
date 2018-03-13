f7df997ad0c7468d10dc84120a56cc10f9a561a9
ngilmore <neal.gilmore@gmail.com>
2018-03-13 12:11:51 -0400

    data preprocessing template

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -33,7 +33,8 @@ list(dataset)
 dataset.dtypes
 
 # Identify missing data
-import missingno as msno
+# pip install missingno
+import missingno as msno # Provides a library of data missingness functions 
 #%matplotlib inline
 msno.matrix(dataset)
 msno.bar(dataset)
@@ -44,7 +45,7 @@ msno.dendrogram(dataset)
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Handle missing data
+# Handle missing data (If necessary)
 from sklearn.preprocessing import Imputer # Imputes numerical variables
 imputer = Imputer(missing_values = 'NaN',
                   strategy = 'mean',
@@ -52,7 +53,7 @@ imputer = Imputer(missing_values = 'NaN',
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-# Encode categorical variables
+# Encode categorical variables (If necessary)
 from sklearn.preprocessing import LabelEncoder # Encodes categorical variables
 from sklearn.preprocessing import OneHotEncoder # Converts categorical variables to dummy variables
 
@@ -67,4 +68,10 @@ y = labelencoder_y.fit_transform(y)
 # Split the dataset into Training and Test sets
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Scale Features (If necessary)
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
 
