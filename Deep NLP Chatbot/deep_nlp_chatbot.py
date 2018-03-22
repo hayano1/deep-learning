a49995ec970e21c15d0cb2228fa137d305de2528
ngilmore <neal.gilmore@gmail.com>
2018-03-21 20:28:40 -0400

    deep nlp chatbot using tensorflow

---
+++ Deep NLP Chatbot/deep_nlp_chatbot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 07:24:15 2018
+
+@author: ngilmore
+"""
+
+# Deep NLP Chatbot using Tensorflow
+
+# Part 0: Environment Setup
+# Create virtual environment in Terminal:
+# sudo conda create -n chatbot python=3.5 anaconda
+# source activate chatbot
+# sudo pip install tensorflow==1.0.0
+
+# Get dataset
+# Google "Cornell Movie Dialogs Corpus"
+# Download the zip file and extract to required folder
+# Select the movie_conversations.txt and movie_lines.txt files
+
+# Open Anaconda
+# Applications on --> chatbot
+# Launch Spyder from here
+
+# Part 1: Data Preprocessing
+# Part 2: Build the Seq2Seq Model
+# Part 3: Train the Seq2Seq Model
+# Part 4: Test the Seq2Seq Model
